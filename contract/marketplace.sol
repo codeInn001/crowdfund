83ed69bcaf40cb1db8a7bfbd3368fedb5552525d
alege oluwasegun <emmaalege@gmail.com>
2022-05-24 16:43:15 +0100

    update

--- contract/marketplace.sol
+++ contract/marketplace.sol
@@ -87,3 +87,155 @@ contract Marketplace {
         return (productsLength);
     }
 }
+
+
+
+
+
+
+
+// SPDX-License-Identifier: MIT  
+
+pragma solidity >=0.7.0 <0.9.0;
+
+interface IERC20Token {
+  function transfer(address, uint256) external returns (bool);
+  function approve(address, uint256) external returns (bool);
+  function transferFrom(address, address, uint256) external returns (bool);
+  function totalSupply() external view returns (uint256);
+  function balanceOf(address) external view returns (uint256);
+  function allowance(address, address) external view returns (uint256);
+
+  event Transfer(address indexed from, address indexed to, uint256 value);
+  event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+contract AmbuMarketPlace {
+
+    uint internal productsLength = 0;
+    uint internal likes;
+    uint number;
+    address internal cUsdTokenAddress = 0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1;
+
+    struct Product {
+        address payable owner;
+        string name;
+        string image;
+        string location;
+        uint price;
+        uint rating;
+        uint services;
+            
+    }
+
+    struct Rating {
+        address payable sender;
+        uint rate;
+        uint numberOfRate;
+        uint average;
+    }
+
+    mapping (uint => Product) internal products;
+    mapping (uint => Rating) internal ratings;
+
+    function writeProduct(
+        string memory _name,
+        string memory _image,
+        string memory _location,
+        uint _price
+    ) public {
+        uint _rating = 10;
+        uint _services = 0;
+        products[productsLength] = Product(
+            payable(msg.sender),
+			_name,
+			_image,
+			_location,
+			_price,
+			_rating,
+			_services
+        );
+
+        productsLength++;
+    }
+
+    function readProduct(uint _index) public view returns (
+		address payable,
+		string memory, 
+		string memory, 
+		string memory, 
+		uint, 
+		uint,
+		uint
+	) {
+		return (
+			products[_index].owner, 
+			products[_index].name, 
+			products[_index].image, 
+			products[_index].location, 
+			products[_index].price,
+			products[_index].rating,
+			products[_index].services
+		);
+	}
+
+
+    function orderAmbulance(uint _index) public payable {
+        require(
+                IERC20Token(cUsdTokenAddress).transferFrom(
+                    msg.sender,
+                    products[_index].owner,
+                    products[_index].price
+            ),
+            "Transfer failed"
+        );
+        products[_index].services++;
+    }
+
+
+    function getProductsLength() public view returns (uint) {
+        return (productsLength);
+    } 
+
+    
+    // function like (uint _index) public {
+
+    //     products[_index].rating += products[_index].rating / 
+    //     products[_index].rating++;
+    // }
+
+    function writeRating(
+        uint _rate,
+        uint _numberOfRate,
+        uint _average
+    ) public {
+        uint _rate = 0;
+        uint _numberOfRate = 0;
+        uint _average = 0;
+
+        ratings[productsLength] = Rating (
+            payable(msg.sender),
+            _rate,
+            _numberOfRate,
+            _average
+        )
+    }
+
+    function readRating(uint _index, uint _selectedRate) public (
+        address payable,
+        uint,
+        uint,
+        uint
+    ) {
+        ratings[_index].rate += selectedRate;
+        numberOfRate[_index].rate++;
+        average[_index].rate = selectedRate/numberOfRate;
+
+        products[_index].rating = average[_index].rate;
+
+        
+    }
+
+     
+}
+
--- src/main.js
+++ src/main.js
@@ -5,13 +5,14 @@ import marketplaceAbi from "../contract/marketplace.abi.json"
 import erc20Abi from "../contract/erc20.abi.json"
 
 const ERC20_DECIMALS = 18
-const MPContractAddress = "0xc8Aa0605CE03FAE52818A6dB21Fc4347Ab99D2d5"
+const MPContractAddress = "0x83e8420e8F953eB7246615D7640307E90e1e5E8F"
 const cUSDContractAddress = "0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1"
 
 let kit
 let contract
-let products = []
+let hospitals = []
 
+// check if celo wallet extension is available in the browser
 const connectCeloWallet = async function () {
   if (window.celo) {
     notification("Please approve this DApp to use it.")
@@ -34,6 +35,8 @@ const connectCeloWallet = async function () {
   }
 }
 
+
+// approve payment
 async function approve(_price) {
   const cUSDContract = new kit.web3.eth.Contract(erc20Abi, cUSDContractAddress)
 
@@ -43,69 +46,87 @@ async function approve(_price) {
   return result
 }
 
+// get buyers account balance
 const getBalance = async function () {
   const totalBalance = await kit.getTotalBalance(kit.defaultAccount)
   const cUSDBalance = totalBalance.cUSD.shiftedBy(-ERC20_DECIMALS).toFixed(2)
   document.querySelector("#balance").textContent = cUSDBalance
 }
 
-const getProducts = async function() {
-  const _productsLength = await contract.methods.getProductsLength().call()
-  const _products = []
-  for (let i = 0; i < _productsLength; i++) {
-    let _product = new Promise(async (resolve, reject) => {
-      let p = await contract.methods.readProduct(i).call()
+// Fetch hospitals from smart contract
+const getHospitals = async function() {
+  const _hospitalsLength = await contract.methods.getHospitalsLength().call()
+  const _hospitals = []
+  for (let i = 0; i < _hospitalsLength; i++) {
+    let _hospital = new Promise(async (resolve, reject) => {
+      let p = await contract.methods.readHospital(i).call()
       resolve({
         index: i,
         owner: p[0],
         name: p[1],
         image: p[2],
-        description: p[3],
-        location: p[4],
-        price: new BigNumber(p[5]),
-        sold: p[6],
+        location: p[3],
+        // location: p[4],
+        price: new BigNumber(p[4]),
+        rating: new BigNumber(p[5]),
+        services: new BigNumber(p[6]),
       })
     })
-    _products.push(_product)
+    _hospitals.push(_hospital)
   }
-  products = await Promise.all(_products)
-  renderProducts()
+  hospitals = await Promise.all(_hospitals)
+  renderHospitals()
 }
 
-function renderProducts() {
+// display our hospitals in the user interface
+function renderHospitals() {
   document.getElementById("marketplace").innerHTML = ""
-  products.forEach((_product) => {
+  hospitals.forEach((_hospital) => {
     const newDiv = document.createElement("div")
     newDiv.className = "col-md-4"
-    newDiv.innerHTML = productTemplate(_product)
+    newDiv.innerHTML = hospitalTemplate(_hospital)
     document.getElementById("marketplace").appendChild(newDiv)
   })
+
 }
 
-function productTemplate(_product) {
+// template for building hospitals user interface
+function hospitalTemplate(_hospital) {
   return `
     <div class="card mb-4">
-      <img class="card-img-top" src="${_product.image}" alt="...">
+      <img class="card-img-top" src="${_hospital.image}" alt="${_hospital.name}">
       <div class="position-absolute top-0 end-0 bg-warning mt-4 px-2 py-1 rounded-start">
-        ${_product.sold} Sold
+        ${_hospital.services} Services
       </div>
+    
       <div class="card-body text-left p-4 position-relative">
         <div class="translate-middle-y position-absolute top-0">
-        ${identiconTemplate(_product.owner)}
+        ${identiconTemplate(_hospital.owner)}
         </div>
-        <h2 class="card-title fs-4 fw-bold mt-2">${_product.name}</h2>
-        <p class="card-text mb-4" style="min-height: 82px">
-          ${_product.description}             
-        </p>
-        <p class="card-text mt-4">
+        <h2 class="card-title fs-4 fw-bold mt-2">${_hospital.name}</h2>
+        <p class="card-text mt-0">
           <i class="bi bi-geo-alt-fill"></i>
-          <span>${_product.location}</span>
+          <span>${_hospital.location}</span>
         </p>
+
+        <div class="rating" data-id=${
+          _hospital.index}>
+          <i class="rating__star far fa-star" data-id="0"></i>
+          <i class="rating__star far fa-star" data-id="1"></i>
+          <i class="rating__star far fa-star" data-id="2"></i>
+          <i class="rating__star far fa-star" data-id="3"></i>
+          <i class="rating__star far fa-star" data-id="4"></i>
+        </div>
+        <p class="card-text mt-4" style="min-height: 30px" id=${
+            _hospital.index}>
+          ${_hospital.rating}/5 rating            
+        </p>
+    
         <div class="d-grid gap-2">
           <a class="btn btn-lg btn-outline-dark buyBtn fs-6 p-3" id=${
-            _product.index
+            _hospital.index
           }>
-            Buy for ${_product.price.shiftedBy(-ERC20_DECIMALS).toFixed(2)} cUSD
+            Buy for ${_hospital.price.shiftedBy(-ERC20_DECIMALS).toFixed(2)} cUSD
           </a>
         </div>
       </div>
@@ -113,6 +134,8 @@ function productTemplate(_product) {
   `
 }
 
+
+// Creates a small image called an identicon
 function identiconTemplate(_address) {
   const icon = blockies
     .create({
@@ -132,66 +155,160 @@ function identiconTemplate(_address) {
   `
 }
 
+// display notification for users
 function notification(_text) {
   document.querySelector(".alert").style.display = "block"
   document.querySelector("#notification").textContent = _text
 }
 
+// hide notifications after it is displayed 
 function notificationOff() {
   document.querySelector(".alert").style.display = "none"
 }
 
+
+// renders our function after windows loads
 window.addEventListener("load", async () => {
-  notification("⌛ Loading...")
+  notification("Loading...")
   await connectCeloWallet()
   await getBalance()
-  await getProducts()
+  await getHospitals()
   notificationOff()
 });
 
+
+// listen to Add button then save new hospital in an array which is pushed to our smart contract
 document
-  .querySelector("#newProductBtn")
+  .querySelector("#newHospitalBtn")
   .addEventListener("click", async (e) => {
+
+    // collect form data in an array and pass as argument to smart contract
     const params = [
-      document.getElementById("newProductName").value,
+      document.getElementById("newHospitalName").value,
       document.getElementById("newImgUrl").value,
-      document.getElementById("newProductDescription").value,
-      document.getElementById("newLocation").value,
+      document.getElementById("newHospitalDescription").value,
       new BigNumber(document.getElementById("newPrice").value)
       .shiftedBy(ERC20_DECIMALS)
-      .toString()
+      .toString(),
     ]
-    notification(`⌛ Adding "${params[0]}"...`)
+    
+    notification(`Adding "${params[0]}..."`)
     try {
       const result = await contract.methods
-        .writeProduct(...params)
+        .writeHospital(...params)
         .send({ from: kit.defaultAccount })
     } catch (error) {
-      notification(`⚠️ ${error}.`)
+      notification(`${error}.`)
     }
-    notification(`🎉 You successfully added "${params[0]}".`)
-    getProducts()
+    notification(` You successfully added "${params[0]}".`)
+    getHospitals()
   })
 
+
+  // listen to buy button and allows users to order ambulance
 document.querySelector("#marketplace").addEventListener("click", async (e) => {
   if (e.target.className.includes("buyBtn")) {
     const index = e.target.id
     notification("⌛ Waiting for payment approval...")
     try {
-      await approve(products[index].price)
+      await approve(hospitals[index].price)
     } catch (error) {
-      notification(`⚠️ ${error}.`)
+      notification(`${error}.`)
     }
-    notification(`⌛ Awaiting payment for "${products[index].name}"...`)
+    notification(` Awaiting payment for order of ambulance from "${hospitals[index].name}..."`)
     try {
       const result = await contract.methods
-        .buyProduct(index)
+        .orderAmbulance(index)
         .send({ from: kit.defaultAccount })
-      notification(`🎉 You successfully bought "${products[index].name}".`)
-      getProducts()
+      notification(`You successfully ordered ambulance from "${hospitals[index].name}".`)
+      getHospitals()
       getBalance()
     } catch (error) {
-      notification(`⚠️ ${error}.`)
+      notification(`${error}.`)
     }
   }
-})  
+}) 
+
+// allow users to search hospitals based on location
+document.querySelector('.btn-outline-success').addEventListener('click', (e) => {
+    e.preventDefault()
+    const searchValue = document.querySelector('.search-bar').value
+    const searchResult = hospitals.filter(hospital => {
+        if(hospital.location.toLowerCase() === searchValue.toLowerCase()) {
+            notificationOff();
+            return hospital;
+        } 
+    })
+
+    renderSearch(searchResult)
+
+    if(searchResult.length <= 0) { 
+
+      notification('result not found')
+  }
+    
+})
+
+// displays search result in the user interface
+function renderSearch(searchResult) {
+    document.getElementById("marketplace").innerHTML = ""
+    searchResult.forEach((_hospital) => {
+      const newDiv = document.createElement("div")
+      newDiv.className = "col-md-4"
+      newDiv.innerHTML = hositalTemplate(_hospital)
+      document.getElementById("marketplace").appendChild(newDiv)
+    })
+  }
+
+  // notification after users uses the search field
+document.querySelector('.search-bar').addEventListener('input', (e) => {
+    // e.preventDefault()
+    if(e.target.value === ""){
+        notificationOff()
+        getHospitals()
+    }
+
+})
+
+
+// allows users to rate hospitals
+document.querySelector('#marketplace').addEventListener('click', async (e) => {
+  
+    if(e.target.className.includes('rating__star')) {
+      const ratingStars = [...e.target.parentElement.children]
+    
+      ratingStars.forEach(star => {
+        
+        if(star.dataset.id <= e.target.dataset.id){
+          
+          star.classList.add('fas')
+          star.classList.remove('far')
+        } else if (star.dataset.id > e.target.dataset.id) {
+          star.classList.remove('fas')
+          star.classList.add('far')
+        }
+      })
+    }
+})
+
+// write users rating to smart contract
+document
+  .querySelector("#marketplace")
+  .addEventListener("click", async (e) => {
+    if(e.target.className.includes('rating__star')) {
+      let id = e.target.parentElement.dataset.id;
+      
+      let selectedRating = (Number(e.target.dataset.id) + 1);
+  
+    try {
+      notification(`rating ambulance service ...`) 
+      const result = await contract.methods
+        .writeRating(id, selectedRating)
+        .send({ from: kit.defaultAccount })
+    } catch (error) {
+      notification(`${error}.`)
+    }
+    notification(`successfully rated ambulance service`)
+    getHospitals()
+    }
+  })
